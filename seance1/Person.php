64558003d7f13a54c6f403ae3e8927d8e01b6c47
Marcos <markitosanches@gmail.com>
2023-12-15 14:49:35 -0500

    Seance 1

--- seance1/Person.php
+++ seance1/Person.php
@@ -1,6 +1,18 @@
 <?php
 
 class Person {
-    $name
+    public int $id;
+    public string $name = 'Paul';
+    public string $address;
 }
+
+$obj = new Person;
+
+$obj->id = 10;
+$obj->name = "Peter";
+$obj->address = "Pie Ix";
+
+//var_dump($obj);
+
+echo $obj->name;
 ?>
