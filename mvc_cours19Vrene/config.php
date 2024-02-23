df8ae453101f734213d4c3de267e4b966c4ca7c6
René de Montigny <e2395944@cmaisonneuve.qc.ca>
2024-02-23 12:33:44 -0500

    Ajouter le cours 19 Login

---
+++ mvc_cours19Vrene/config.php
@@ -0,0 +1,3 @@
+<?php
+define('BASE', '/htdSession_H23_24/php/githubProf/web_avancee_23602/mvc_cours19Vrene');
+define('ASSET', '/htdSession_H23_24/php/githubProf/web_avancee_23602/mvc_cours19Vrene/public');
